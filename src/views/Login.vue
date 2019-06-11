0ea086f0a10dfaee8934c55809f029dc16d9fe67
Brandon Phillips <brandonkeithphillips@gmail.com>
2019-06-11 15:12:55 +0000

    updated crisis button to be more legible on small screens

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,41 +1,39 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-1">
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    required
-                    prepend-icon="fas fa-mail-bulk"
-                    solo
-                    name="login"
-                    label="Login"
-                    type="email"
-                  ></v-text-field>
-                  <v-text-field
-                    required
-                    prepend-icon="fas fa-key"
-                    solo
-                    name="password"
-                    label="Password"
-                    id="password"
-                    type="password"
-                  ></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn flat color="primary">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-1">
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  required
+                  prepend-icon="fas fa-mail-bulk"
+                  solo
+                  name="login"
+                  label="Login"
+                  type="email"
+                ></v-text-field>
+                <v-text-field
+                  required
+                  prepend-icon="fas fa-key"
+                  solo
+                  name="password"
+                  label="Password"
+                  id="password"
+                  type="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat color="primary">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
