4a7973efcec8c70fcb58ddbe04156741ee446491
Brandon Phillips <brandonkeithphillips@gmail.com>
2019-07-02 20:09:06 +1200

    final commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,6 +28,44 @@
         </v-card>
       </v-flex >
     </v-layout>
+    
+    <v-layout justify-center> 
+      <v-flex xs12 md8 lg4 pa-2>
+        <v-card dark color="info" height="100%">
+          <v-card-title class="headline justify-center font-weight-bold">
+            Journal Count
+            </v-card-title>
+          <v-card-text class="body font-weight-light-italic">
+            You have 
+            {{
+              // is userJournals truthy? ie: exists or has a value
+              this.userJournals ? Object.keys(this.userJournals).length : 0
+            }}
+            journal entries saved.
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 md8 lg4 pa-2>
+        <v-card height="100%">
+          <v-card-title class="headline justify-center font-weight-bold">
+            Like our app?
+          </v-card-title>
+          <v-card-text>
+            Feel free to contribute your own functionality to our application with a Vue component! <br />
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn 
+              href="https://github.com/epione-app/epione" 
+              color="primary"
+            >
+              <v-icon left>fab fa-github-square</v-icon>
+              <span>Fork us</span>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
     <v-layout justify-center>
       <v-flex xs12 md8 pa-2>
         <v-card dark color="ternary">
@@ -113,43 +151,6 @@
           <v-card-actions>
             <v-spacer />
             <span class="caption font-weight-bold">Information retrieved from Youthline</span>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout justify-center> 
-      <v-flex xs12 md8 lg4 pa-2>
-        <v-card dark color="info" height="100%">
-          <v-card-title class="headline justify-center font-weight-bold">
-            Journal Count
-            </v-card-title>
-          <v-card-text class="body font-weight-light-italic">
-            You have 
-            {{
-              // is userJournals truthy? ie: exists or has a value
-              this.userJournals ? Object.keys(this.userJournals).length : 0
-            }}
-            journal entries saved.
-          </v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex xs12 md8 lg4 pa-2>
-        <v-card height="100%">
-          <v-card-title class="headline justify-center font-weight-bold">
-            Like our app?
-          </v-card-title>
-          <v-card-text>
-            Feel free to contribute your own functionality to our application with a Vue component! <br />
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn 
-              href="https://github.com/epione-app/epione" 
-              color="primary"
-            >
-              <v-icon left>fab fa-github-square</v-icon>
-              <span>Fork us</span>
-            </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
